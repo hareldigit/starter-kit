46be7e2a405d851d6438e0c2a7792914119f549a
Harel Dagan <harel.dagan1@gmail.com>
2025-02-04 21:00:49 +0200

    add "shared" path alias configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
+import {resolve} from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-    },
-  },
+      '@shared': resolve(__dirname, '../shared')
+    }
+  }
 })
