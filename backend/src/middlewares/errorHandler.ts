fd9aa8698a466d19baaf57b75219770a94ee15ee
Harel Dagan <harel.dagan1@gmail.com>
2025-02-25 17:26:28 +0200

    infra: add local ci  (husky,lint-staged)

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,11 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const errorHandler = (
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const errorHandler = (err: Error, req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
   res.status(500).json({ message: 'Internal Server Error' });
 };
